fb806f0fd2179388151dc5ccc8c34254e73f74f3
Andrei <misael_marin@dlsu.edu.ph>
2024-03-28 01:15:16 +0800

    update

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
-const userRoutes = require("./lib/routes/userRoutes");
-
+const userRoutes = require("./routes/userRoutes");
+const setupDbSubscription = require("./lib/middleware/dbSetup");
 const port = 3006;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+// middleware and routes...
+
+// setup database subscription
+setupDbSubscription();
 
 app.use("/user", userRoutes);
 
@@ -15,7 +20,6 @@ app.get("/", (req, res) => {
   res.send("Welcome to my server!");
 });
 
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-}); 
+});
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const userController = require("../controllers/userController");
-
+const testController = require("../controllers/testController");
 router.get("/", userController.viewAccount);
+router.get("/getMaps", userController.getMapping);
 
+router.post("/postMap", userController.postMapping);
+
+router.post("/uploadEncounter", testController.uploadEncounter);
+router.post("/generateKey", testController.generateApiKey);
+router.post("/requestApproval", testController.requestApproval);
+router.post("/getRequests", testController.getPatientRequests);
+router.post("/updateRequestStatus", testController.updateRequest);
 module.exports = router;
